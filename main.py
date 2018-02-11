6b5bc8be469f142cc9bb68eaeaafbf7876771fc0
Umar Yusuf <forum2k9@gmail.com>
2018-02-11 22:37:09 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,40 @@
+import pandas as pd
+from geopy.geocoders import Nominatim, ArcGIS, GoogleV3
 from flask import Flask, render_template, request
+# import requests
 
 app = Flask(__name__)
 
 
-@app.route( '/')
+@app.route('/')
 def index():
 	return render_template("index.html")
 
 
-@app.route('/index_2')
-def index_2():
-	url_name = 'https://www.w3schools.com/'
-	return render_template("index_2.html", url_name=url_name)
 
 
+@app.route("/r_geocoder", methods=['GET', 'POST'])
+def r_geocoder():
+	try:
+		my_lat = float(request.form['mylat'])
+		my_long = float(request.form['mylong'])
+	except Exception:
+		my_lat = None
+		my_long = None
+
+	nom = ArcGIS()
+
+	if (my_lat or my_long) == None:
+		address = "Invalid Lat/Long values"
+		return render_template("r_geocoder.html", my_lat=my_lat, my_long=my_long, address=address)
+	else:
+		try:
+			# n = nom.reverse((32.839097, -96.663127), timeout=10) # Lat, Long
+			n = nom.reverse((my_lat, my_long), timeout=10) # Lat, Long
+			address = n.address
+			return render_template("r_geocoder.html", my_lat=my_lat, my_long=my_long, address=address)
+		except Exception:
+			return render_template("r_geocoder.html", my_lat=my_lat, my_long=my_long, address="No Address for the provided Lat/Long values!")
 
 if __name__ == '__main__':
 	app.run(debug=True)
